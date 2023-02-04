f5666495450e70c2528d494828a564cdda52275e
ArsDi <abstractlevl@gmail.com>
2023-02-04 12:35:56 +0300

    triangle basic realization

---
+++ dmitriev.arseny/T1/triangle.h
@@ -0,0 +1,30 @@
+#ifndef TROANGLE
+#define TRIANGLE
+
+#include "shape.h"
+
+class Triangle : public Shape
+{
+public:
+	Triangle(point_t a, point_t b, point_t c);
+
+	double getArea() override;
+	rectangle_t getFrameRect() override;
+	void move(double x, double y) override;
+	void move(point_t pos) override;
+	void scale(double k) override;
+	Shape* clone() const override;
+
+private:
+	point_t a;
+	point_t b;
+	point_t c;
+
+	point_t center;
+
+	double aSide;
+	double bSide;
+	double cSide;
+};
+
+#endif
