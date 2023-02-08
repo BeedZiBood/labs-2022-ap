d89ddb5b887a5d707f46241ba9d799da36da8210
xXdogx <ilya-likhanov@mail.ru>
2023-02-08 19:04:17 +0300

    fixed CG

--- lihanov.ilya/I1/LOCALMAX.cpp
+++ lihanov.ilya/I1/LOCALMAX.cpp
@@ -6,7 +6,7 @@ void LocalMax::operator()(unsigned int currentValue)
 {
   const unsigned int MaxUnInt = std::numeric_limits< unsigned int >::max();
  
- if (currentValue != 0 && beforePreviousValue != 0 && previousValue != 0) {
+  if (currentValue != 0 && beforePreviousValue != 0 && previousValue != 0) {
       if (count == MaxUnInt) {
           throw std::overflow_error("Sequence too long. Error occurred.");
       }
