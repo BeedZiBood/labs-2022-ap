164af3de2317d0a47a9bd6c17625dea536c2dcdb
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-11 15:15:48 +0300

    complete main

--- potapova.marina/I4/main.cpp
+++ potapova.marina/I4/main.cpp
@@ -14,39 +14,82 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  const std::uint16_t matrix_order = inputMatrixOrder(argv[1]);
-
-  std::uint32_t* spiral_matrix;
-
-  try
+  size_t count_rows = 0;
+  size_t count_cols = 0;
+  std::ifstream input(argv[2]);
+  if (!input.is_open())
   {
-    spiral_matrix = new std::uint32_t[matrix_order * matrix_order];
-  }
-  catch(const std::bad_alloc& e)
-  {
-    std::cerr << "Allocation failed: " << e.what() << '\n';
+    std::cerr << "File cannot be open\n";
 
     return 1;
   }
 
-  fillSpiralMatrix(spiral_matrix, matrix_order);
-  printSpiralMatrix(spiral_matrix, matrix_order, argv[2]);
+  input >> count_rows >> count_cols;
+  if (!input)
+  {
+    std::cerr << "File is empty\n";
 
-  delete[] spiral_matrix;
+    return 1;
+  }
 
-  size_t count_rows;
-  size_t count_cols;
-  std::int64_t* matrix;
+  if (count_rows * count_cols > 1000)
+  {
+    std::cerr << "Incorrect range";
 
-  inputMatrix(count_rows, count_cols, matrix, argv[1]);
+    return 1;
+  }
 
-  printCountOfSaddlePoints(
-      getCountOfSaddlePoints(matrix, count_rows, count_cols),
-      argv[2]);
+  if (!std::strcmp(argv[1], "1"))
+  {
+    int arr[1000];
+    for (size_t i = 0; i < count_rows * count_cols; ++i)
+    {
+      input >> arr[i];
+      if (!input)
+      {
+        std::cerr << "Error\n";
 
-  delete[] matrix;
+        return 1;
+      }
+    }
+    std::ofstream output(argv[3]);
+    output << countRowsWithoutNull(arr, count_rows, count_cols) << " " << countRowsWithoutNull(arr, count_rows, count_cols) << "\n";
+  }
+  else
+  {
+    const std::uint16_t matrix_order = inputMatrixOrder(argv[1]);
 
-  
+    std::uint32_t* spiral_matrix;
+
+    try
+    {
+      spiral_matrix = new std::uint32_t[matrix_order * matrix_order];
+    }
+    catch(const std::bad_alloc& e)
+    {
+      std::cerr << "Allocation failed: " << e.what() << '\n';
+
+      return 1;
+    }
+
+    fillSpiralMatrix(spiral_matrix, matrix_order);
+    printSpiralMatrix(spiral_matrix, matrix_order, argv[2]);
+
+    delete[] spiral_matrix;
+
+    size_t count_rows;
+    size_t count_cols;
+    std::int64_t* matrix;
+
+    inputMatrix(count_rows, count_cols, matrix, argv[1]);
+
+    printCountOfSaddlePoints(
+        getCountOfSaddlePoints(matrix, count_rows, count_cols),
+        argv[2]);
+
+    delete[] matrix;
+    }
+
 
   return 0;
 }
