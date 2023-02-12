adabcc7b91a4ad5445ce68ba0af9ce67699f53f1
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-12 18:12:43 +0300

    main function is completed TODO: function for checking real numbers

--- potapova.marina/I5/main.cpp
+++ potapova.marina/I5/main.cpp
@@ -1,4 +1,19 @@
+#include "workWithStrs.h"
+#include "checkRealNumber.h"
+#include "workWithIO.h"
+
 int main()
 {
+    const char* const input_str = stdinGetLine();
 
+    if (input_str == nullptr)
+    {
+        std::cerr << "Empty input\n";
+
+        return 1;
+    }
+    std::cout << std::boolalpha << checkRealNumber(input_str) << "\n";
+    delete[] input_str;
+
+    return 0;
 }
