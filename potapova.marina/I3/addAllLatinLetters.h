1d84a57bf7f465e0abcaf5dad0779432475fc579
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-19 15:53:22 +0300

    add I3

--- potapova.marina/I3/addAllLatinLetters.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADDALLLATINLETTERS_H
-#define ADDALLLATINLETTERS_H
-
-void addAllLatinLettersFromStr(char*& dest_end_ptr, const char* str);
-char* addAllLatinLetters(char* dest, const char* str1, const char* str2);
-
-#endif
--- potapova.marina/I3/getMostCommonSyms.h
+++
@@ -1,6 +0,0 @@
-#ifndef GETMOSTCOMMONSYMS_H
-#define GETMOSTCOMMONSYMS_H
-
-char* getMostCommonSyms(char* dest, const char* str);
-
-#endif
