42413dff2a2a3697aa135ec1efb63133bf7e9806
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-24 11:37:37 +0300

    add work with Atan function TODO: add exceptions

--- potapova.marina/I6/calculateAtanSum.cpp
+++ potapova.marina/I6/calculateAtanSum.cpp
@@ -2,11 +2,18 @@
 
 double calculateAtanSum(double x, double abs_error, size_t num_max)
 {
-
-}
-
-void printTable(double abs_error, size_t num_max, double start,
-                double end, double step)
-{
-
+    double numerator = x;
+    double summation = x;
+    int sign = 1;
+    int num_summation = 1;
+    double sum_res = 0;
+    while (fabs(summation) > abs_error)
+    {
+        summation = sign * numerator / (2 * num_summation - 1);
+        sum_res += summation;
+        numerator *= x * x;
+        sign = -sign;
+        num_summation++;
+    }
+    return sum_res;
 }
--- potapova.marina/I6/calculateAtanSum.h
+++ potapova.marina/I6/calculateAtanSum.h
@@ -2,9 +2,9 @@
 #define CALCULATEATANSUM_H
 
 #include <cstddef>
+#include <cmath>
+#include <stdexcept>
 
 double calculateAtanSum(double x, double abs_error, size_t num_max);
-void printTable(double abs_error, size_t num_max, double start,
-                double end, double step);
 
 #endif
