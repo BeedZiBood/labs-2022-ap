3080d9fce8b591889c3d1d6abd983ffc141a23b8
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-24 11:41:48 +0300

    empty print function

---
+++ potapova.marina/I6/printTable.cpp
@@ -0,0 +1,7 @@
+#include "printTable.h"
+#include "calculateAtanSum.h"
+
+void printTable(double abs_error, size_t num_max, double start, double end, double step)
+{
+
+}
---
+++ potapova.marina/I6/printTable.h
@@ -0,0 +1,10 @@
+#ifndef PRINTTABLE_H
+#define PRINTTABLE_H
+
+#include <cstddef>
+#include <iostream>
+#include <iomanip>
+
+void printTable(double abs_error, size_t num_max, double start, double end, double step);
+
+#endif
