ca938354e086c47e5eab946e6158fda252ce336f
Marinochka.n_n <marina-potapova04@mail.ru>
2023-02-24 11:45:38 +0300

    add input TODO: add function with Atan

--- potapova.marina/I6/main.cpp
+++ potapova.marina/I6/main.cpp
@@ -1,4 +1,20 @@
-int main()
-{
+#include "calculateAtanSum.h"
+#include "printTable.h"
 
+int main(int argc, const char* argv[])
+{
+    double start, end;
+    double abs_error;
+    size_t num_max;
+    double step;
+    std::cout << "Input interval: ";
+    std::cin >> start >> end;
+    std::cout << "Input step: ";
+    std::cin >> step;
+    std::cout << "Input abs error: ";
+    std::cin >> abs_error;
+    std::cout << "Input maximum terms number: ";
+    std::cin >> num_max;
+
+    return 0;
 }
