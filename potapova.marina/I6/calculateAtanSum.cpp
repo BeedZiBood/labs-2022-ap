d6c5e6419e564a9f59e86fd5a135200f3aa5ef28
Marinochka.n_n <marina-potapova04@mail.ru>
2023-03-01 23:59:00 +0300

    add "data header" + CG TODO: print answer

--- potapova.marina/I6/calculateAtanSum.cpp
+++ potapova.marina/I6/calculateAtanSum.cpp
@@ -11,7 +11,7 @@ double calculateAtanSum(double x, double abs_error, size_t num_max)
     double numerator = x;
     double summation = x;
     int sign = 1;
-    int num_summation = 1;
+    size_t num_summation = 1;
     double sum_res = 0;
     while (fabs(summation) > abs_error)
     {
--- potapova.marina/I6/printTable.cpp
+++ potapova.marina/I6/printTable.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
+#include <iomanip>
 #include "printTable.h"
 #include "calculateAtanSum.h"
 
 void printTable(double abs_error, size_t num_max, double start, double end, double step)
 {
-
+    std::cout << std::setw(3) << "N" << std::setw(10) << "x"
+              << std::setw(12) << "sum(x)" << std::setw(12) << "atan(x)" << '\n';
+    size_t n = (end - start) / step + 1;
+    double x = start;
 }
