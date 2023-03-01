6fe996fad53b7d862b7e8960de0d14b2e7becc7c
Marinochka.n_n <marina-potapova04@mail.ru>
2023-03-02 00:01:14 +0300

    add print answer

--- potapova.marina/I6/printTable.cpp
+++ potapova.marina/I6/printTable.cpp
@@ -9,4 +9,14 @@ void printTable(double abs_error, size_t num_max, double start, double end, doub
               << std::setw(12) << "sum(x)" << std::setw(12) << "atan(x)" << '\n';
     size_t n = (end - start) / step + 1;
     double x = start;
+    for (size_t i = 1; i <= n; i++)
+    {
+        double s = calculateAtanSum(x, abs_error, num_max);
+        double y = atan(x);
+        std::cout << std::fixed << std::setprecision(4)
+                  << std::setw(3) << i << std::setw(10) << x
+                  << std::fixed << std::setprecision(6)
+                  << std::setw(12) << s << std::setw(12) << y << '\n';
+        x += step;
+    }
 }
