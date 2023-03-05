b19e53b20b0f7444edbe4a253cc25bf6e7a602a9
Vavericherry <vaverich3@inbox.ru>
2023-03-05 16:30:55 +0300

    Add the function which detect the all chars of cstring

---
+++ averich.vladimir/I3/allcharsofcstrings.cpp
@@ -0,0 +1,63 @@
+#include "allcharsofcstrings.hpp"
+#include "sortcharsbyascii.hpp"
+#include <cstddef>
+#include <cctype>
+#include <iostream>
+int allCharsOfCstrings(char* destination, const char* cstring, const char* cstringInside)
+{
+  size_t sizeOfDestination = 0;
+  size_t notLonelyChar = 0;
+  for (size_t i = 0; cstringInside[i]; i++)
+  {
+    if (std::isalpha(cstringInside[i]))
+    {
+      for (size_t j = i + 1; cstringInside[j]; j++)
+      {
+        if (std::tolower(cstringInside[i]) == std::tolower(cstringInside[j]))
+        {
+          notLonelyChar = 1;
+        }
+      }
+      for (size_t j = 0; cstringInside[j]; j++)
+      {
+        if (std::tolower(cstringInside[i]) == std::tolower(cstringInside[j]))
+        {
+          notLonelyChar = 1;
+        }
+      }
+      if (!notLonelyChar)
+      {
+        destination[sizeOfDestination++] = std::tolower(cstringInside[i]);
+      }
+    }
+  }
+  for (size_t i = 0, notLonelyChar = 0; cstring[i]; i++, notLonelyChar = 0)
+  {
+    if (std::isalpha(cstring[i]))
+    {
+      for (size_t j = i + 1; cstring[j]; j++)
+      {
+        if (std::tolower(cstring[i]) == std::tolower(cstring[j]))
+        {
+          notLonelyChar = 1;
+        }
+      }
+      if (!notLonelyChar)
+      {
+        destination[sizeOfDestination++] = std::tolower(cstring[i]);
+      }
+    }
+  }
+  for (size_t i = 0; i < sizeOfDestination; i++)
+  {
+    for (size_t j = i + 1; j < sizeOfDestination; j++)
+    {
+      if (destination[i] > destination[j])
+      {
+        std::swap(destination[i], destination[j]);
+      }
+    }
+  }
+  destination[sizeOfDestination] = '\0';
+  return 0;
+}
---
+++ averich.vladimir/I3/allcharsofcstrings.hpp
@@ -0,0 +1,4 @@
+#ifndef ALLCHARSOFCSTRINGS_HPP
+#define ALLCHARSOFCSTRINGS_HPP
+int allCharsOfCstrings(char* destination, const char* cstring, const char* cstringInside);
+#endif
