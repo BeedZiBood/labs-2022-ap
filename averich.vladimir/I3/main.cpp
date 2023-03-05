8764581ec24359bd5924184ae48219080ce6a110
Vavericherry <vaverich3@inbox.ru>
2023-03-05 15:57:49 +0300

    Added the read string function to the main.cpp with exception

--- averich.vladimir/I3/main.cpp
+++ averich.vladimir/I3/main.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
 #include <cstddef>
+#include <readthestring.hpp>
 int main()
 {
   size_t size = 0;
   char* cstring = nullptr;
   size_t increaseCapacity = 10;
+  try
+  {
+    cstring = readTheString(std::cin, size, increaseCapacity);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << '\n';
+    return 1;
+  }
   char* destination = nullptr;
   char cstringInside[] = "ab";
   delete[] cstring;
