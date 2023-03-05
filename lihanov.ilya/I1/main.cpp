d7ac157c504ec8d1674c6c14e8fa2714a9f6b470
xXdogx <ilya-likhanov@mail.ru>
2023-03-05 16:33:34 +0300

    zzzzzzz

--- lihanov.ilya/I1/main.cpp
+++ lihanov.ilya/I1/main.cpp
@@ -11,23 +11,23 @@ int main() {
     if (!std::cin) {
       std::cout << "Error occurred." << "/n";
       return 1;
-      }
-      try {
-        LocalMax(currentValue);
-      }
-      catch (const std::overflow_error& e) {
-  	std::cout << e.what() << "/n";
-  	return 2;
-      }
-      try {
-  	Equal(currentValue);
-      }
-      catch (const std::overflow_error& e) {
-  	std::cout << e.what() << "/n";
-  	return 2;
-      }
+    }
+    try {
+      LocalMax(currentValue);
+    }
+    catch (const std::overflow_error& e) {
+      std::cout << e.what() << "/n";
+      return 2;
+    }
+    try {
+      Equal(currentValue);
+    }
+    catch (const std::overflow_error& e) {
+      std::cout << e.what() << "/n";
+      return 2;
+    }
 
-  } while (currentValue && std::cin);
+    } while (currentValue && std::cin);
 
   std::cout << "Maximum successive equal elements number: " << LocalMax.count << "\n";
   std::cout << "Strict local maxima number: " << Equal.countV << "\n";
