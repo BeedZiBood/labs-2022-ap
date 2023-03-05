f855024ee4e5cb3bfca3bde5499eabcbd630e610
xXdogx <ilya-likhanov@mail.ru>
2023-03-05 16:05:03 +0300

    z

--- lihanov.ilya/I1/main.cpp
+++ lihanov.ilya/I1/main.cpp
@@ -6,21 +6,26 @@ int main() {
   Equal Equal{0 , 0 , 0};
   unsigned int currentValue = 0;
   do {
-      std::cin >> currentValue;
+    std::cin >> currentValue;
 
-      if (!std::cin) {
-          std::cout << "Error occurred." << "/n";
-          return 1;
-      }
+    if (!std::cin) {
+        std::cout << "Error occurred." << "/n";
+        return 1;
+    }
 
-      try {
-          LocalMax(currentValue);
-      }
-      catch (const std::overflow_error& e) {
-          std::cout << e.what() << "/n";
-          return 2;
-      }
-
+    try {
+        LocalMax(currentValue);
+    }
+    catch (const std::overflow_error& e) {
+        std::cout << e.what() << "/n";
+        return 2;
+    }
+    try {
+      Equal(currentValue);
+    }
+    catch (const std::overflow_error& e) {
+      std::cout << e.what() << "/n";
+      return 2;
   } while (currentValue && std::cin);
 
   std::cout << "Maximum successive equal elements number: " << LocalMax.count << "\n";
