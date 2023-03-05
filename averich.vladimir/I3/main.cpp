daf7d6fc40ae48f4bffe9443196d8d89e79149b6
Vavericherry <vaverich3@inbox.ru>
2023-03-05 15:40:22 +0300

    Add the destination, size for the functions, capacity for cstring and etc.

--- averich.vladimir/I3/main.cpp
+++ averich.vladimir/I3/main.cpp
@@ -1,4 +1,12 @@
+#include <cstddef>
 int main()
 {
-
+  size_t size = 0;
+  char* cstring = nullptr;
+  size_t increaseCapacity = 10;
+  char* destination = nullptr;
+  char cstringInside[] = "ab";
+  delete[] cstring;
+  delete[] destination;
+  return 0; 
 }
