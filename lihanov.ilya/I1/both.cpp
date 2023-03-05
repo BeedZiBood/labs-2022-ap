828dd1a93021c664c8954a27b48bdf84e93dac49
xXdogx <ilya-likhanov@mail.ru>
2023-03-05 20:25:46 +0300

    bruh

--- lihanov.ilya/I1/both.cpp
+++ lihanov.ilya/I1/both.cpp
@@ -6,11 +6,11 @@ void LocalMax::operator()(unsigned int currentValue) {
   const unsigned int MaxUnInt = std::numeric_limits< unsigned int >::max();
 
   if (currentValue != 0 && beforePreviousValue != 0 && previousValue != 0) {
-      if (count == MaxUnInt) {
-          throw std::overflow_error("Sequence too long. Error occurred.");
-      }
+    if (count == MaxUnInt) {
+      throw std::overflow_error("Sequence too long. Error occurred.");
+    }
 
-      count += (previousValue > currentValue && previousValue > beforePreviousValue);
+    count += (previousValue > currentValue && previousValue > beforePreviousValue);
   }
   beforePreviousValue = previousValue;
   previousValue = currentValue;
@@ -19,11 +19,11 @@ void Equal::operator()(unsigned int currentValue){
   const unsigned int MaxUnInt = std::numeric_limits< unsigned int >::max();
 
   if (currentValue != 0 && beforePreviousValue != 0 && previousValue != 0) {
-      if (countV == MaxUnInt) {
-          throw std::overflow_error("Sequence too long. Error occurred.");
-      }
-      countV += (previousValue > currentValue && previousValue < beforePreviousValue);
-  }
+    if (countV == MaxUnInt) {
+      throw std::overflow_error("Sequence too long. Error occurred.");
+    }
+    countV += (previousValue > currentValue && previousValue < beforePreviousValue);
+}
 
   beforePreviousValue = previousValue;
   previousValue = currentValue;
