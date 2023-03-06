c627b2c65896c3779d7145a58b4e41e2866122ec
Царева ЯД <tsarewaya@yandex.ru>
2023-03-06 22:57:32 +0300

    make basefun

---
+++ tsareva.yana/T1/base_functions.h
@@ -0,0 +1,9 @@
+#ifndef BASE_FUNCTIONS_H
+#define BASE_FUNCTIONS_H
+#include "base_types.h"
+namespace baseFunctions
+{
+  point_t findSumm(point_t point, double dx, double dy);
+  point_t findDifference()
+}
+#endif
