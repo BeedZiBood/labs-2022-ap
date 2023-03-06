06417c5a3d0e6fec2b617a2d9c3dc2b31c339739
Царева ЯД <tsarewaya@yandex.ru>
2023-03-06 23:15:06 +0300

    make throw

--- tsareva.yana/T1/rectangle.cpp
+++ tsareva.yana/T1/rectangle.cpp
@@ -1,9 +1,13 @@
 #include "rectangle.h"
+#include <stdexcept>
 Rectangle::Rectangle(point_t left_down, point_t right_up):
   left_down_(left_down),
   right_up_(right_up)
 {
-
+  if (left_down_.x >= right_up_.x || left_down_.y >= right_up_.y)
+  {
+    throw std::invalid_argyment("Error rectangle parameters");
+  }
 }
 
 double Rectangle::getArea() const
