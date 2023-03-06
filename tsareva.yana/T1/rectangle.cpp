70bf9d5d7b10c83d4bb0ff425bb5b56ab93716dc
Царева ЯД <tsarewaya@yandex.ru>
2023-03-06 23:47:18 +0300

    ((((

--- tsareva.yana/T1/rectangle.cpp
+++ tsareva.yana/T1/rectangle.cpp
@@ -6,8 +6,18 @@ Rectangle::Rectangle(point_t left_down, point_t right_up):
 {
   if (left_down_.x >= right_up_.x || left_down_.y >= right_up_.y)
   {
-    throw std::invalid_argyment("Error rectangle parameters");
+    throw std::invalid_argument("Error rectangle parameters");
   }
+}
+
+
+void Rectangle::scale(double k)
+{
+  if (k <= 0)
+  {
+    throw std::invalid_argument("Error ratio");
+  }
+
 }
 
 double Rectangle::getArea() const
