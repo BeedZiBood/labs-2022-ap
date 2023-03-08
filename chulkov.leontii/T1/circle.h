e542e9bc6a1e1e639a2b121c34a0b553a46aff55
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-08 23:37:12 +0300

    add circle.h

---
+++ chulkov.leontii/T1/circle.h
@@ -0,0 +1,19 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#include "shape.h"
+
+class Circle: public Shape
+{
+  public:
+    Circle(const point_t& pos, double radius);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t& pos) override;
+    void move(const double dx, const double dy) override;
+    void scale(double k) override;
+
+  private:
+    point_t pos_;
+    double radius_;
+};
+#endif
