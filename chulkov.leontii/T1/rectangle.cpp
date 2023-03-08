eecb1085ba14aadece0874e68bc9b7dee6c8ebc2
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-08 23:43:41 +0300

    add constructor and exceptions in rectangle.cpp

---
+++ chulkov.leontii/T1/rectangle.cpp
@@ -0,0 +1,13 @@
+#include "rectangle.h"
+#include <stdexcept>
+
+Rectangle::Rectangle(const double width, const double height, const point_t& pos):
+  width_(width),
+  height_(height),
+  pos_(pos)
+{
+  if (width_ < 0.0 || height_ < 0.0)
+  {
+    throw std::invalid_argument("Error: Invalid rectangle dimensions");
+  }
+}
