68800f347e61c0806d4e5aafb33cb52b5b72a025
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-08 23:33:57 +0300

    add shape.h

---
+++ chulkov.leontii/T1/shape.h
@@ -0,0 +1,16 @@
+#ifndef SHAPE_HP
+#define SHAPE_HP
+#include "basetype.h"
+
+class Shape
+{
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const point_t& pos) = 0;
+    virtual void move(const double dx, const double dy) = 0;
+    virtual void scale(double k) = 0;
+    virtual Shape* clone() const = 0;
+};
+#endif
