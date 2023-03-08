93dd4dd5889e20c431ecf187373178df65b6fbf9
Kozyrin Semen <garbagemilitia@gmail.com>
2023-03-08 15:10:09 +0300

    Implemented Rectangle

---
+++ kozyrin.semen/T1/rectangle.cpp
@@ -0,0 +1,45 @@
+#include "rectangle.h"
+#include <stdexcept>
+
+Rectangle::Rectangle(point_t p1, point_t p2):
+  rect_{p1.x - p2.x, p1.y - p2.y, (p1.x - p2.x) / 2, (p1.y - p2.y) / 2}
+{
+  if (rect_.width <= 0 || rect_.height <= 0) {
+    throw std::invalid_argument("Incorrect rectangle size");
+  }
+}
+
+Rectangle::Rectangle(rectangle_t rect):
+  rect_(rect)
+{}
+
+double Rectangle::getArea() const
+{
+  return rect_.width * rect_.height;
+}
+
+rectangle_t Rectangle::getFrameRect() const
+{
+  return rect_;
+}
+
+void Rectangle::move(point_t point)
+{
+  rect_.pos = point;
+}
+
+void Rectangle::move(double dx, double dy)
+{
+  addVector(rect_.pos, dx, dy);
+}
+
+void Rectangle::scale(double k)
+{
+  rect_.width *= k;
+  rect_.height *= k;
+}
+
+Shape* Rectangle::clone()
+{
+  return new Rectangle(rect_);
+}
---
+++ kozyrin.semen/T1/rectangle.h
@@ -0,0 +1,19 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "shape.h"
+
+class Rectangle: public Shape {
+public:
+  Rectangle(point_t p1, point_t p2);
+  explicit Rectangle(rectangle_t rect);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(point_t point) override;
+  void move(double dx, double dy) override;
+  void scale(double k) override;
+  Shape* clone() override;
+private:
+  rectangle_t rect_;
+};
+
+#endif
