eb3a99f3cf17e459292d4cab65c375fe5989b731
Vavericherry <vaverich3@inbox.ru>
2023-03-09 23:29:22 +0300

    Fix the CG

--- averich.vladimir/common/extendthestring.cpp
+++ averich.vladimir/common/extendthestring.cpp
@@ -3,7 +3,7 @@
 #include <stdexcept>
 #include <cstddef>
 char* extendTheString(char* cstring, size_t& size, size_t& capacity)
-{ 
+{
   char* newstring = new char[capacity];
   char* j = newstring;
   for (const char* i = cstring; i != cstring + size; ++i, ++j)
