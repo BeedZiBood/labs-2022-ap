35138c4fa45836aa37c145d22f26332481b559e4
Vavericherry <vaverich3@inbox.ru>
2023-03-09 23:27:57 +0300

    Fix common

---
+++ averich.vladimir/common/readthearray.cpp
@@ -0,0 +1,17 @@
+#include "readthearray.hpp"
+#include <iostream>
+#include <fstream>
+#include <cstddef>
+#include <cctype>
+std::istream& readTheArray(std::istream& in, size_t sizeOfFileArray, int* fileArray)
+{
+  for (size_t i = 0; i < sizeOfFileArray; i++)
+  {
+    in >> fileArray[i];
+    if (!in)
+    {
+      return in;
+    }
+  }
+  return in;
+}
