b11320af8d59541578ce7a7d54fb6caa79dc4c8f
Царева ЯД <tsarewaya@yandex.ru>
2023-03-10 00:37:31 +0300

    make constructor parallelogram

--- tsareva.yana/T1/parallelogram.cpp
+++ tsareva.yana/T1/parallelogram.cpp
@@ -1,1 +1,13 @@
 #include "parallelogram.h"
+#include <stdexcept>
+
+Parallelogram::Parallelogram(point_t first, point_t second, point_t third):
+  first_(first),
+  second_(second),
+  third_(third)
+{
+  if(!((first_.y == second_.y) || (second_.y == third_.y)))
+  {
+    throw std::invalid_argument("Error parallelogram parameters");
+  }
+}
