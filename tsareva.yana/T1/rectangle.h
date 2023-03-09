fb08c87d9e591efafa06f570d24968c6f43b88e5
Царева ЯД <tsarewaya@yandex.ru>
2023-03-09 20:04:48 +0300

    fix 2

--- tsareva.yana/T1/rectangle.h
+++ tsareva.yana/T1/rectangle.h
@@ -6,7 +6,7 @@ class Rectangle: public Shape
 public:
   Rectangle(point_t left_down, point_t right_up);
   double getArea() const override;
-  rectangle_t gerFrameRectangle() const;
+  rectangle_t getFrameRectangle() const override;
   void move(point_t position) override;
   void move(double dx, double dy) override;
   void scale(double k) override;
