7f59f8796c31ff0f69374abc4e5a98abc0e79c12
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-10 23:35:05 +0300

    add print.cpp

---
+++ chulkov.leontii/T1/print.cpp
@@ -0,0 +1,20 @@
+#include "print.h"
+#include <iostream>
+
+void printTable(const CompositeShape& cShp)
+{
+  for (size_t i = 0; i < cShp.size(); ++i) {
+    double minX = cShp[i]->getFrameRect().pos_.x;
+    minX -= cShp[i]->getFrameRect().width_ / 2;
+    double minY = cShp[i]->getFrameRect().pos_.y;
+    minY -= cShp[i]->getFrameRect().height_ / 2;
+    double maxX = cShp[i]->getFrameRect().pos_.x;
+    maxX += cShp[i]->getFrameRect().width_ / 2;
+    double maxY = cShp[i]->getFrameRect().pos_.y;
+    maxY += cShp[i]->getFrameRect().height_ / 2;
+    std::cout << " " << minX;
+    std::cout << " " << minY;
+    std::cout << " " << maxX;
+    std::cout << " " << maxY;
+  }
+}
