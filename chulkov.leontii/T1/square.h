fee8ae155772415d250f59607b8b46527b3e3974
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-10 23:31:52 +0300

    correction in square.h

--- chulkov.leontii/T1/square.h
+++ chulkov.leontii/T1/square.h
@@ -1,19 +1,20 @@
 #ifndef SQUARE_H
 #define SQUARE_H
-#include "basetype.h"
 #include "shape.h"
+#include "rectangle.h"
 
-class Square: public Shape {
-public:
-  Square(const point_t& center, double side);
-  double getArea() const override;
-  rectangle_t getFrameRect() const override;
-  void move(const point_t& newCenter) override;
-  void move(double dx, double dy) override;
-  void scale(double k) override;
+class Square: public Shape
+{
+  public:
+    Square(const point_t& pos, double side);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t& newCenter) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+    Shape* clone() const;
 
-private:
-  point_t center_;
-  double side_;
+  private:
+    Rectangle rect_;
 };
 #endif
