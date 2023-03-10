10ced176bd23308ce37b7571e97490b0bb4412ee
Царева ЯД <tsarewaya@yandex.ru>
2023-03-10 13:27:14 +0300

    class diamond

---
+++ tsareva.yana/T1/diamond.cpp
@@ -0,0 +1,9 @@
+#include "diamond.h"
+
+Diamond::Diamond(point_t first, point_t second, point_t third):
+  first_(first),
+  second_(second),
+  third_(third)
+{
+
+}
--- tsareva.yana/T1/diamond.h
+++ tsareva.yana/T1/diamond.h
@@ -1,10 +1,10 @@
-#ifndef PARALLELOGRAM_H
-#define PARALLELOGRAM_H
+#ifndef DIAMOND_H
+#define DIAMOND_H
 #include "shape.h"
-class Parallelogram: public Shape
+class Diamond: public Shape
 {
 public:
-  Parallelogram(point_t first, point_t second, point_t third);
+  Diamond(point_t first, point_t second, point_t third);
   double getArea() const override;
   rectangle_t getFrameRectangle() const override;
   void move(point_t position) override;
