9ce0942b75572c0e036ed50e9155d562b8ae2833
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-10 23:39:38 +0300

    add compositeshape.h

---
+++ chulkov.leontii/T1/compositeshape.h
@@ -0,0 +1,33 @@
+#ifndef COMPOSITESHAPE_H
+#define COMPOSITESHAPE_H
+#include <cstddef>
+#include "basetype.h"
+#include "shape.h"
+
+class CompositeShape
+{
+  public:
+    CompositeShape();
+    CompositeShape(const CompositeShape& anotherCompShp);
+    CompositeShape(CompositeShape&& anotherCompShp);
+    ~CompositeShape();
+    CompositeShape& operator=(const CompositeShape& anotherCompShp);
+    CompositeShape& operator=(CompositeShape&& anotherCompShp);
+
+    double getArea() const;
+    rectangle_t getFrameRect() const;
+    void move(point_t position);
+    void move(double dx, double dy);
+    void scale(double k);
+    void isotropScale(point_t pos, double k);
+
+    void pushBack(Shape* shp);
+    Shape* operator[](size_t id) const;
+    bool empty() const;
+    size_t size() const;
+
+  private:
+    size_t size_;
+    Shape** shp_;
+};
+#endif
