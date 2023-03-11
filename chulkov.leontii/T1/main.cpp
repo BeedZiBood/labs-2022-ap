dc78ae63ab67cdd73db886b0b2f3d67327a458d0
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-12 00:59:36 +0300

    removing extra if and correcting in main.cpp

--- chulkov.leontii/T1/main.cpp
+++ chulkov.leontii/T1/main.cpp
@@ -22,103 +22,100 @@ int main()
       std::cerr << "Error input\n";
       return 1;
     }
-    if (name == "RECTANGLE" || name == "SQUARE" || name == "CIRCLE")
+    try
     {
-      try
+      if (name == "RECTANGLE")
       {
-        if (name == "RECTANGLE")
+        use = true;
+        try
         {
-          use = true;
+          double x = 0.0;
+          double y = 0.0;
+          std::cin >> x >>y;
+          point_t a{x, y};
+          std::cin >> x >> y;
+          point_t b{x, y};
+          if (a.x > b.x || a.y > b.y)
+          {
+            throw std::logic_error("Bad scale");
+          }
+          if (!std::cin)
+          {
+            throw std::invalid_argument("invalid arguments");
+          }
           try
           {
-            double x = 0.0;
-            double y = 0.0;
-            std::cin >> x >>y;
-            point_t a{x, y};
-            std::cin >> x >> y;
-            point_t b{x, y};
-            if (a.x > b.x || a.y > b.y)
-            {
-              throw std::logic_error("Bad scale");
-            }
-            if (!std::cin)
-            {
-              throw std::invalid_argument("invalid arguments");
-            }
-            try
-            {
-              Rectangle* rect = new Rectangle(a, b);
-              compositeShape.pushBack(rect);
-            }
-            catch (const std::exception& e)
-            {
-              std::cerr << e.what() << "\n";
-            }
-          }
-          catch (const std::logic_error& e)
-          {
-            std::cerr << e.what() << "\n";
-          }
-        }
-        else if (name == "SQUARE")
-        {
-          use = true;
-          try
-          {
-            double x = 0.0;
-            double y = 0.0;
-            double z = 0.0;
-            std::cin >> x >> y >> z;
-            if (!std::cin)
-            {
-              throw std::invalid_argument("invalid arguments");
-            }
-            try
-            {
-              Square* square = new Square(point_t {x, y}, z);
-              compositeShape.pushBack(square);
-            }
-            catch (const std::exception& e)
-            {
-              std::cerr << e.what() << "\n";
-            }
-          }
-          catch (const std::logic_error& e)
-          {
-            std::cerr << e.what() << "\n";
-          }
-        }
-        else if (name == "CIRCLE")
-        {
-          use = true;
-          try
-          {
-            double x = 0.0;
-            double y = 0.0;
-            double r = 0.0;
-            std::cin >> x >> y >> r;
-            try
-            {
-              Circle* circle = new Circle({x, y}, r);
-              compositeShape.pushBack(circle);
-            }
-            catch (const std::exception& e)
-            {
-              std::cerr << e.what() << "\n";
-            }
+            Rectangle* rect = new Rectangle(a, b);
+            compositeShape.pushBack(rect);
           }
           catch (const std::exception& e)
           {
             std::cerr << e.what() << "\n";
           }
         }
+        catch (const std::logic_error& e)
+        {
+          std::cerr << e.what() << "\n";
+        }
       }
-      catch (const std::invalid_argument &e)
+      else if (name == "SQUARE")
       {
-        std::cerr << e.what() << "\n";
+        use = true;
+        try
+        {
+          double x = 0.0;
+          double y = 0.0;
+          double z = 0.0;
+          std::cin >> x >> y >> z;
+          if (!std::cin)
+          {
+            throw std::invalid_argument("invalid arguments");
+          }
+          try
+          {
+            Square* square = new Square(point_t {x, y}, z);
+            compositeShape.pushBack(square);
+          }
+          catch (const std::exception& e)
+          {
+            std::cerr << e.what() << "\n";
+          }
+        }
+        catch (const std::logic_error& e)
+        {
+          std::cerr << e.what() << "\n";
+        }
+      }
+      else if (name == "CIRCLE")
+      {
+        use = true;
+        try
+        {
+          double x = 0.0;
+          double y = 0.0;
+          double r = 0.0;
+          std::cin >> x >> y >> r;
+          try
+          {
+            Circle* circle = new Circle({x, y}, r);
+            compositeShape.pushBack(circle);
+          }
+          catch (const std::exception& e)
+          {
+            std::cerr << e.what() << "\n";
+          }
+        }
+        catch (const std::exception& e)
+        {
+          std::cerr << e.what() << "\n";
+        }
       }
     }
-    else if (name == "SCALE")
+    catch (const std::invalid_argument &e)
+    {
+      std::cerr << e.what() << "\n";
+    }
+    if (name == "SCALE")
     {
       try
       {
@@ -133,11 +130,11 @@ int main()
         double k = 0.0;
         std::cin >> k;
         std::cout << (compositeShape.getArea());
-        printTable(compositeShape);
+        printResulte(compositeShape);
         std::cout << "\n";
         compositeShape.isotropScale(point, k);
         std::cout << compositeShape.getArea();
-        printTable(compositeShape);
+        printResulte(compositeShape);
         std::cout << "\n";
       }
       catch (const std::invalid_argument& e)
