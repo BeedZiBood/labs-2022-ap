01f6481071c27d75c3d509516defc242aaa4456d
Царева ЯД <tsarewaya@yandex.ru>
2023-03-11 19:26:51 +0300

    make findModule fun

--- tsareva.yana/T1/base_types.cpp
+++ tsareva.yana/T1/base_types.cpp
@@ -1,4 +1,5 @@
 #include "base_types.h"
+#include <cmath>
 
 point_t movePoint(point_t point, point_t d_point)
 {
@@ -9,3 +10,21 @@ point_t getCenterParallelogram(point_t first_top, point_t second_top)
 {
   return { (first_top.x + second_top.x) / 2, (first_top.y + second_top.y) / 2 };
 }
+
+double findModule(point_t first_top, point_t second_top)
+{
+  return std::sqrt(std::pow(first_top.x - second_top.x, 2) + std::pow(first_top.y - second_top.y, 2));
+}
+
+bool isTriangle(point_t first_top, point_t second_top, point_t third_top)
+{
+  double first_side = findModule(first_top, second_top);
+  double second_side = findModule(first_top, third_top);
+  double third_side = findModule(second_top, third_top);
+  if ((first_side + second_side > third_side) && (first_side + third_side > second_side) && (second_side + third_side > first_side))
+  {
+    return true;
+  }
+  return false;
+}
+
--- tsareva.yana/T1/base_types.h
+++ tsareva.yana/T1/base_types.h
@@ -14,5 +14,7 @@ struct rectangle_t
 };
 
 point_t movePoint(point_t point, point_t d_point);
-point_t getCenterParallelogram(point_t first_top, point_t second_top)
+point_t getCenterParallelogram(point_t first_top, point_t second_top);
+bool isTriangle(point_t first_top, point_t second_top, point_t third_top);
+double findModule(point_t first_top, point_t second_top);
 #endif
