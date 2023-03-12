5f5b51f798313f850dd94b51cae3cd9d1ce62cd0
Вадим Гришин <vadimgrishin@MacBook-Pro-Vadim-2.local>
2023-03-12 20:05:05 +0300

    change headers

--- grishin.vadim/I1/LocalMax.h
+++ grishin.vadim/I1/LocalMax.h
@@ -1,5 +1,5 @@
+#ifndef LOCALMAX_H
+#define LOCALMAX_H
 #include "iostream"
-#ifndef UNTITLED1_LOCALMAX_H
-#define UNTITLED1_LOCALMAX_H
 int localMax (int countLocalMax, int currentValue, int value1, int value2);
 #endif
--- grishin.vadim/I1/sequence.h
+++ grishin.vadim/I1/sequence.h
@@ -1,5 +1,5 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
 #include "iostream"
-#ifndef UNTITLED1_SEQUENCE_H
-#define UNTITLED1_SEQUENCE_H
 int sequence(int count2, int currentValue2, int value1, int value2);
 #endif
