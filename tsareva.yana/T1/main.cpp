b8390a33a511df2c4a318f16ce3a75b957714bdd
Царева ЯД <tsarewaya@yandex.ru>
2023-03-14 00:27:05 +0300

    make shapes

--- tsareva.yana/T1/main.cpp
+++ tsareva.yana/T1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstddef>
 #include "shape.h"
 #include "rectangle.h"
 #include "parallelogram.h"
@@ -8,6 +9,9 @@ int main()
 {
   std::string figure;
   std::cin >> figure;
+  size_t size = 0;
+  size_t capacity = 10;
+  Shape ** shapes = new Shape*[capacity];
   if (figure == "RECTANGLE")
   {
     double x1 = 0.0;
@@ -16,6 +20,8 @@ int main()
     double y2 = 0.0;
     std::cin >> x1 >> y1 >> x2 >> y2;
     Rectangle * rectangle = new Rectangle({x1, y1}, {x2, y2});
+    shapes[size] = rectangle;
+    size++;
   }
   else if (figure == "PARALLELOGRAM")
   {
@@ -27,6 +33,8 @@ int main()
     double y3 = 0.0;
     std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     Parallelogram * parallelogram = new Parallelogram({x1, y1}, {x2, y2}, {x3, y3});
+    shapes[size] = parallelogram;
+    size++;
   }
   else if (figure == "DIAMOND")
   {
@@ -38,6 +46,8 @@ int main()
     double y3 = 0.0;
     std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     Diamond * diamond = new Diamond({x1, y1}, {x2, y2}, {x3, y3});
+    shapes[size] = diamond;
+    size++;
   }
   return 0;
 }
