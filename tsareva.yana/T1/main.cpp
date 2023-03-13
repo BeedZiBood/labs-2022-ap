276da1d5928021fd6fda17e5987c2d6e5d5448df
Царева ЯД <tsarewaya@yandex.ru>
2023-03-14 00:30:45 +0300

    std::cin figure

--- tsareva.yana/T1/main.cpp
+++ tsareva.yana/T1/main.cpp
@@ -8,46 +8,49 @@
 int main()
 {
   std::string figure;
-  std::cin >> figure;
   size_t size = 0;
   size_t capacity = 10;
   Shape ** shapes = new Shape*[capacity];
-  if (figure == "RECTANGLE")
+  while (std::cin)
   {
-    double x1 = 0.0;
-    double x2 = 0.0;
-    double y1 = 0.0;
-    double y2 = 0.0;
-    std::cin >> x1 >> y1 >> x2 >> y2;
-    Rectangle * rectangle = new Rectangle({x1, y1}, {x2, y2});
-    shapes[size] = rectangle;
-    size++;
-  }
-  else if (figure == "PARALLELOGRAM")
-  {
-    double x1 = 0.0;
-    double x2 = 0.0;
-    double y1 = 0.0;
-    double y2 = 0.0;
-    double x3 = 0.0;
-    double y3 = 0.0;
-    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    Parallelogram * parallelogram = new Parallelogram({x1, y1}, {x2, y2}, {x3, y3});
-    shapes[size] = parallelogram;
-    size++;
-  }
-  else if (figure == "DIAMOND")
-  {
-    double x1 = 0.0;
-    double x2 = 0.0;
-    double y1 = 0.0;
-    double y2 = 0.0;
-    double x3 = 0.0;
-    double y3 = 0.0;
-    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-    Diamond * diamond = new Diamond({x1, y1}, {x2, y2}, {x3, y3});
-    shapes[size] = diamond;
-    size++;
+    std::cin >> figure;
+    if (figure == "RECTANGLE")
+    {
+      double x1 = 0.0;
+      double x2 = 0.0;
+      double y1 = 0.0;
+      double y2 = 0.0;
+      std::cin >> x1 >> y1 >> x2 >> y2;
+      Rectangle *rectangle = new Rectangle({x1, y1}, {x2, y2});
+      shapes[size] = rectangle;
+      size++;
+    }
+    else if (figure == "PARALLELOGRAM")
+    {
+      double x1 = 0.0;
+      double x2 = 0.0;
+      double y1 = 0.0;
+      double y2 = 0.0;
+      double x3 = 0.0;
+      double y3 = 0.0;
+      std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+      Parallelogram *parallelogram = new Parallelogram({x1, y1}, {x2, y2}, {x3, y3});
+      shapes[size] = parallelogram;
+      size++;
+    }
+    else if (figure == "DIAMOND")
+    {
+      double x1 = 0.0;
+      double x2 = 0.0;
+      double y1 = 0.0;
+      double y2 = 0.0;
+      double x3 = 0.0;
+      double y3 = 0.0;
+      std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+      Diamond *diamond = new Diamond({x1, y1}, {x2, y2}, {x3, y3});
+      shapes[size] = diamond;
+      size++;
+    }
   }
   return 0;
 }
