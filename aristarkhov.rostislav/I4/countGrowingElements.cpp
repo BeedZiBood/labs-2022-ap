b43af4d4cb06a599349c9f72c49900e2792d03ce
Aristarkhov Rostislav <aristarhov.ro@edu.spbstu.ru>
2023-03-14 21:39:17 +0300

    fix function

--- aristarkhov.rostislav/I4/countGrowingElements.cpp
+++ aristarkhov.rostislav/I4/countGrowingElements.cpp
@@ -1,21 +1,25 @@
 #include "countGrowingElements.h"
-#include <iostream>
+
+bool isGrowingElements(const int* matrix, size_t line, size_t column)
+{
+  size_t counter_in_row = 0;
+  for (size_t j = 0; j < column; j++)
+  {
+    size_t order1 = line * column + j;
+    if (matrix[order1] < matrix[order1 + 1])
+    {
+      counter_in_row++;
+    }
+  }
+  return counter_in_row == column - 1;
+}
 
 size_t countGrowingElements(const int* matrix, size_t line, size_t column)
 {
   size_t counter = 0;
   for (size_t i = 0; i < line; i++)
   {
-    size_t counter_in_line = 0;
-    for (size_t j = 0; j < column - 1; j++)
-    {
-      size_t order1 = i * column + j;
-      if (matrix[order1] < matrix[order1 + 1])
-      {
-        counter_in_line++;
-      }
-    }
-    if (counter_in_line == column - 1)
+    if (isGrowingElements(matrix, i, column))
     {
       counter++;
     }
