58ca9dfed518c8720cf933054c8d9fe95e356b70
Вадим Гришин <vadimgrishin@MacBook-Pro-Vadim-2.local>
2023-03-14 17:53:36 +0300

    delete files

--- grishin.vadim/I1/isLocalMax.h
+++
@@ -1,5 +0,0 @@
-#ifndef ISLOCALMAX_H
-#define ISLOCALMAX_H
-#include <iostream>
-int isLocalMax(int currentValue, int value1, int value2);
-#endif
--- grishin.vadim/I1/isSequence.h
+++
@@ -1,5 +0,0 @@
-#ifndef ISSEQUENCE_H
-#define ISSEQUENCE_H
-#include <iostream>
-int isSequence(int currentValue, int value1, int value2);
-#endif
