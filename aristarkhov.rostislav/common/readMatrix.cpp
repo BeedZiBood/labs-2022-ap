c2cf06194a10251905fd10863fd4c32bc6ec4fbd
Aristarkhov Rostislav <aristarhov.ro@edu.spbstu.ru>
2023-03-14 22:52:06 +0300

    fix readMatrix

--- aristarkhov.rostislav/common/readMatrix.cpp
+++ aristarkhov.rostislav/common/readMatrix.cpp
@@ -9,6 +9,6 @@ int* readMatrix(int* matrix, size_t size_1, std::ifstream& fileInput)
     {
       throw std::length_error("Problems while reading file\n");
     }
-    return matrix;
   }
+  return matrix;
 }
