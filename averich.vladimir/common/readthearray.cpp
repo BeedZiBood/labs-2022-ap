baebe2634b51165fd249c37423b463b35b1d7c4f
Vavericherry <vaverich3@inbox.ru>
2023-03-16 02:21:50 +0300

    Fix build

--- averich.vladimir/common/readthearray.cpp
+++ averich.vladimir/common/readthearray.cpp
@@ -1,11 +1,5 @@
 #include "readthearray.hpp"
-<<<<<<< HEAD
-#include <iostream>
-#include <fstream>
-#include <cstddef>
-#include <cctype>
-=======
->>>>>>> master
+
 std::istream& readTheArray(std::istream& in, size_t sizeOfFileArray, int* fileArray)
 {
   for (size_t i = 0; i < sizeOfFileArray; i++)
--- averich.vladimir/common/readthearray.hpp
+++ averich.vladimir/common/readthearray.hpp
@@ -1,10 +1,6 @@
 #ifndef READTHEARRAY_HPP
 #define READTHEARRAY_HPP
-<<<<<<< HEAD
-#include <fstream>
-=======
 #include <iostream>
->>>>>>> master
 #include <cstddef>
 std::istream& readTheArray(std::istream& in, size_t sizeOfFileArray, int* fileArray);
 #endif
