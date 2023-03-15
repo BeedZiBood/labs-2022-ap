aa83f6a7cb21258d22ee946643db02774f7e75d3
Потапова <marina-potapova04@mail.ru>
2023-03-15 17:42:17 +0300

    fix CG

--- potapova.marina/I6/calculateAtanSum.cpp
+++ potapova.marina/I6/calculateAtanSum.cpp
@@ -4,26 +4,26 @@
 
 double calculateAtanSum(double x, double abs_error, size_t num_max)
 {
-    if (fabs(x) > 1)
+  if (fabs(x) > 1)
+  {
+    throw std::domain_error("Wrong x interval");
+  }
+  double numerator = x;
+  double summation = x;
+  int sign = 1;
+  size_t num_summation = 1;
+  double sum_res = 0;
+  while (fabs(summation) > abs_error)
+  {
+    if (num_summation > num_max)
     {
-        throw std::domain_error("Wrong x interval");
+      throw std::logic_error("The num_summation doesn't converge");
     }
-    double numerator = x;
-    double summation = x;
-    int sign = 1;
-    size_t num_summation = 1;
-    double sum_res = 0;
-    while (fabs(summation) > abs_error)
-    {
-        if (num_summation > num_max)
-        {
-            throw std::logic_error("The num_summation doesn't converge");
-        }
-        summation = sign * numerator / (2 * num_summation - 1);
-        sum_res += summation;
-        numerator *= x * x;
-        sign = -sign;
-        num_summation++;
-    }
-    return sum_res;
+    summation = sign * numerator / (2 * num_summation - 1);
+    sum_res += summation;
+    numerator *= x * x;
+    sign = -sign;
+    num_summation++;
+  }
+  return sum_res;
 }
--- potapova.marina/I6/main.cpp
+++ potapova.marina/I6/main.cpp
@@ -4,38 +4,36 @@
 
 int main(int argc, const char* argv[])
 {
-    double start, end;
-    double abs_error;
-    size_t num_max;
-    double step;
-    std::cout << "Input interval: ";
-    std::cin >> start >> end;
-    std::cout << "Input step: ";
-    std::cin >> step;
-    std::cout << "Input abs error: ";
-    std::cin >> abs_error;
-    std::cout << "Input maximum terms number: ";
-    std::cin >> num_max;
-    if (!std::cin)
-    {
-        std::cerr << "Wrong input\n";
-        return 1;
-    }
-    if (start > end || step <= 0)
-    {
-        std::cerr << "Wrong interval\n";
-        return 1;
-    }
-
-    try
-    {
-        printTable(abs_error, num_max, start, end, step);
-    }
-    catch (std::logic_error& e)
-    {
-        std::cerr << "Error: " << e.what() << '\n';
-        return 1;
-    }
-
-    return 0;
+  double start, end;
+  double abs_error;
+  size_t num_max;
+  double step;
+  std::cout << "Input interval: ";
+  std::cin >> start >> end;
+  std::cout << "Input step: ";
+  std::cin >> step;
+  std::cout << "Input abs error: ";
+  std::cin >> abs_error;
+  std::cout << "Input maximum terms number: ";
+  std::cin >> num_max;
+  if (!std::cin)
+  {
+    std::cerr << "Wrong input\n";
+    return 1;
+  }
+  if (start > end || step <= 0)
+  {
+    std::cerr << "Wrong interval\n";
+    return 1;
+  }
+  try
+  {
+    printTable(abs_error, num_max, start, end, step);
+  }
+  catch (std::logic_error& e)
+  {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
+  }
+  return 0;
 }
