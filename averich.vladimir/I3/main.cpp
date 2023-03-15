5556dfdcd6812b4a3599b010b8051b9aa1c6ebdd
Vavericherry <vaverich3@inbox.ru>
2023-03-16 00:57:00 +0300

    Fix CH

--- averich.vladimir/I3/main.cpp
+++ averich.vladimir/I3/main.cpp
@@ -10,21 +10,16 @@ int main()
   if (!inputString) {
     return EXIT_FAILURE;
   }
-  
   char* resultOfDuplicateTestString = new char[LettersInEnglishAlphabet + 1];
   if (!resultOfDuplicateTestString) {
     delete [] inputString;
     return EXIT_FAILURE;
   }
-  
   const char predifinedString[] = "abZ";
   findDuplicateLetters(resultOfDuplicateTestString, inputString, predifinedString);
-  
   std::cout << "All chars of cstring: " << resultOfDuplicateTestString << '\n';
   std::cout << "Repeating chars: " << std::boolalpha << hasDoubledChars(inputString) << '\n';
-  
   delete[] inputString;
   delete[] resultOfDuplicateTestString;
-  
   return EXIT_SUCCESS;
 }
