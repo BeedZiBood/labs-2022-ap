271fa88c1293ac4d79592853be794c86ee061269
alexg_29 <alexander.gar2901@gmail.com>
2023-03-16 15:34:49 +0300

    I4 LAB!

---
+++ garzonnavarrete.alexander/I4/SumMaxInDiag.h
@@ -0,0 +1,5 @@
+#ifndef SPBSPU_LABS_2022_AAP_904_A_STRINGPYQ_H_SUMMAXINDIAG_H
+#define SPBSPU_LABS_2022_AAP_904_A_STRINGPYQ_H_SUMMAXINDIAG_H
+#include <cstddef>
+int findMaxSumOfDiagonal(const int* matrix, size_t rows, size_t cols);
+#endif
