fb5c4f3821bdfc94ca737fc1f3396d39d3a0dcbd
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-17 03:14:40 +0300

    Lab 5 Agente Agente Arresteme

--- roblesgonzalez.guillermo/I5/findRealNumber.cpp
+++ roblesgonzalez.guillermo/I5/findRealNumber.cpp
@@ -1,27 +1,27 @@
 #include "findRealNumber.h"
 #include <cctype>
-const char *skipSign(const char* string)
+const char *skipSign(const char *string)
 {
     if (*string == '+' || *string == '-') {
         return string + 1;
     }
     return string;
 }
-const char *skipUnsigned(const char* string)
+const char *skipUnsigned(const char *string)
 {
     if (!isdigit(*(string++))) {
         return nullptr;
     }
     return isdigit(*string) ? skipUnsigned(string) : string;
 }
-const char *skipExponent(const char* string)
+const char *skipExponent(const char *string)
 {
     if (string == nullptr || *(string++) != 'E') {
         return nullptr;
     }
     return skipUnsigned(skipSign(string));
 }
-const char *skipMantissa(const char* string)
+const char *skipMantissa(const char *string)
 {
     string = skipUnsigned(string);
     if (string == nullptr || *string != '.')
@@ -30,7 +30,7 @@ const char *skipMantissa(const char* string)
     }
     return skipUnsigned(string + 1);
 }
-const char *skipRealNumber(const char* string)
+const char *skipRealNumber(const char *string)
 {
     return skipExponent(skipMantissa(skipSign(string)));
 }
