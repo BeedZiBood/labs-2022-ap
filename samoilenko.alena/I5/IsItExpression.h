e526d64301416198d00293072f8163d5a5825192
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-17 18:06:52 +0300

    main and header are ready

--- samoilenko.alena/I5/IsItExpression.h
+++ samoilenko.alena/I5/IsItExpression.h
@@ -1,3 +1,16 @@
 #ifndef ISITEXPRESSION_H
 #define ISITEXPRESSION_H
+#include <cstddef>
+
+bool isLetter(char);
+bool isDigit(char);
+bool isIdentificator(char);
+bool isAster(char);
+bool isSign (char);
+bool isBracket(char);
+bool isUnsignedInteger(const char*, size_t, size_t);
+bool isMult(const char*, size_t, size_t);
+bool isTerm(const char*, size_t, size_t);
+bool examExpress(const char*, size_t, size_t);
+bool outResult(const char*);
 #endif
--- samoilenko.alena/I5/main.cpp
+++ samoilenko.alena/I5/main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <string_from_input.h>
+#include "IsItExpression.h"
+
 int main()
 {
-
+  char* inp = nullptr;
+  try
+  {
+    inp = makeStringFromInput(std::cin);
+    if (inp[0] == '\0')
+    {
+      throw std::logic_error("String is empty");
+    }
+  }
+  catch(const std::exception &ex)
+  {
+    std::cerr << ex.what() << '\n';
+    delete[] inp;
+    return 1;
+  }
+  std::cout << std::boolalpha << outResult(inp) << '\n';
+  delete inp;
+  return 0;
 }
