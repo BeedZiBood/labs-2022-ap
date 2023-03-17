9c9aa995d4625663a302b7bd04c2a4bec3ecbd6d
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-17 16:08:49 +0300

    func for checking for a digit is ready

--- samoilenko.alena/I5/IsItExpression.cpp
+++ samoilenko.alena/I5/IsItExpression.cpp
@@ -1,7 +1,12 @@
 #include "IsItExpression.h"
+#include <iostream>
 
 bool isLetter(char c)
 {
   return c == 'A' || c == 'B' || c == 'C' || c == 'D' || c == 'E';
 }
 
+bool isDigit(char c)
+{
+  return std::isdigit(c);
+}
