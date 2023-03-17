dd80ee7c4d5ac24328d46447db1aaaf238d93579
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-17 17:51:13 +0300

    funcs are ready

--- samoilenko.alena/I5/IsItExpression.cpp
+++ samoilenko.alena/I5/IsItExpression.cpp
@@ -1,5 +1,6 @@
 #include "IsItExpression.h"
 #include <iostream>
+#include <cstring>
 
 bool isLetter(char c)
 {
@@ -41,3 +42,79 @@ bool isUnsignedInteger(const char* inp, size_t firstVal, size_t secondVal)
   return 0;
 }
 
+bool isMult(const char* inp, size_t firstVal, size_t secondVal)
+{
+  if (isIdentificator(*(inp + firstVal)) && (firstVal == secondVal))
+  {
+    return 1;
+  }
+  else
+  {
+    if (isBracket(*(inp + firstVal)) && (isBracket(*(inp + secondVal))))
+    {
+      return examExpress(inp, firstVal + 1, secondVal - 1);
+    }
+    else
+    {
+      return isUnsignedInteger(inp, firstVal, secondVal);
+    }
+  }
+  return 0;
+}
+
+bool isTerm(const char* inp, size_t firstVal, size_t secondVal)
+{
+  size_t t = firstVal;
+  while (!isAster(*(inp + t)) && (t != secondVal))
+  {
+    t++;
+  }
+  if (t == secondVal)
+  {
+    if (isMult(inp, firstVal, t))
+    {
+      return 1;
+    }
+  }
+  if (isMult(inp, firstVal, t - 1))
+  {
+    firstVal = t + 1;
+    return isTerm(inp, firstVal, secondVal);
+  }
+  else
+  {
+    return 0;
+  }
+  return 0;
+}
+
+bool examExpress(const char* inp, size_t firstVal, size_t secondVal)
+{
+  size_t t = firstVal;
+  while (!isSign(*(inp + t)) && (t != secondVal))
+  {
+    t++;
+  }
+  if (t == secondVal)
+  {
+    if (isTerm(inp, firstVal, t))
+    {
+      return 1;
+    }
+  }
+  if (isTerm(inp, firstVal, t - 1))
+  {
+    firstVal = t + 1;
+    return examExpress(inp, firstVal, secondVal);
+  }
+  else
+  {
+    return 0;
+  }
+  return 0;
+}
+
+bool outResult(const char* inp)
+{
+  return examExpress(inp, 0, strlen(inp) - 1);
+}
