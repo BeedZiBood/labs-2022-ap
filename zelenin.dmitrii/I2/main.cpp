268192f1a14969325d1943e24da8f02c879e8b7e
Зеленин Дмитрий <mitya.zelenin12@mail.ru>
2023-03-17 20:46:39 +0300

    delete 2 unused variable

--- zelenin.dmitrii/I2/main.cpp
+++ zelenin.dmitrii/I2/main.cpp
@@ -12,10 +12,8 @@ int main(int argc, char* argv[])
     std::cout << "Erorr!";
     return 1;
   }
-  size_t count_elements_for_array_from_file = 0;
   size_t count_elements = 0;
   size_t count_elements_for_slide_array = 0;
-  char value_for_reading_file = 0;
   size_t max_decreasing_fragment = 0;
   int array_static[7] = { 7,-5,3,8,9,1,-6 };
   isArrayOutput(array_static, 7);
