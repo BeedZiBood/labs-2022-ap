858830da1257b61ce6d67c30caa89d637c894891
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-03-18 23:50:00 +0300

    changed step

--- samoilenko.alena/I6/main.cpp
+++ samoilenko.alena/I6/main.cpp
@@ -6,7 +6,7 @@ int main()
   double left = 0.0;
   double right = 0.0;
   const double absError = 0.0001;
-  const double step = 0.01;
+  const double step = 0.1;
   unsigned numberMax = 0;
   std::cin >> left >> right >> numberMax;
   if (!std::cin)
