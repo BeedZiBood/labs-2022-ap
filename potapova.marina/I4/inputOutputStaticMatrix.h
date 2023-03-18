5511b5f58f317e4502f81a487c37b38ed5c926cc
Потапова <marina-potapova04@mail.ru>
2023-03-18 19:58:24 +0300

    remove extrs files

--- potapova.marina/I4/inputOutputStaticMatrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef INPUTOUTPUTSTATICMATRIX_H
-#define INPUTOUTPUTSTATICMATRIX_H
-
-#include <cstddef>
-#include <fstream>
-
-void inputStaticMatrix(int* matrix, size_t size, std::ifstream& input);
-
-#endif
