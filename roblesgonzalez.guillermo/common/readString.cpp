598e8f01e946f7ad0334da13282555e69c7e0c6b
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-20 23:39:35 +0300

    Lab 5 omg it worked, added customCopy

--- roblesgonzalez.guillermo/common/readString.cpp
+++ roblesgonzalez.guillermo/common/readString.cpp
@@ -5,50 +5,50 @@
 
 void customCopy(char *dest, const char *src, size_t count)
 {
-    for (size_t i = 0; i <= count; ++i)
+  for (size_t i = 0; i <= count; ++i)
+  {
+    dest[i] = src[i];
+    if (src[i] == '\0')
     {
-        dest[i] = src[i];
-        if (src[i] == '\0')
-        {
-            break;
-        }
+      break;
     }
+  }
 }
 char *readString(std::istream &inp)
 {
-    size_t capacity = 10;
-    size_t size = 0;
-    char *cstring = nullptr;
-    cstring = new char[capacity];
-    char chInp = '\0';
-    inp >> std::noskipws;
-    for (inp >> chInp; (inp && chInp) && (chInp != '\n'); inp >> chInp)
+  size_t capacity = 10;
+  size_t size = 0;
+  char *cstring = nullptr;
+  cstring = new char[capacity];
+  char chInp = '\0';
+  inp >> std::noskipws;
+  for (inp >> chInp; (inp && chInp) && (chInp != '\n'); inp >> chInp)
+  {
+    if (size + 1 == capacity)
     {
-        if (size + 1 == capacity)
-        {
-            capacity = capacity + 10;
-            char *dupStr = nullptr;
-            try
-            {
-                dupStr = new char[capacity];
-            }
-            catch (const std::bad_alloc& e)
-            {
-                delete[] cstring;
-                throw;
-            }
-            cstring[size] = '\0';
-            customCopy(dupStr, cstring, size);
-            delete[] cstring;
-            cstring = dupStr;
-        }
-        cstring[size++] = chInp;
-    }
-    if (size == 0)
-    {
+      capacity = capacity + 10;
+      char *dupStr = nullptr;
+      try
+      {
+        dupStr = new char[capacity];
+      }
+      catch (const std::bad_alloc& e)
+      {
         delete[] cstring;
-        throw std::logic_error("Error: Empty String!");
+        throw;
+      }
+      cstring[size] = '\0';
+      customCopy(dupStr, cstring, size);
+      delete[] cstring;
+      cstring = dupStr;
     }
-    cstring[size] = '\0';
-    return cstring;
+    cstring[size++] = chInp;
+  }
+  if (size == 0)
+  {
+    delete[] cstring;
+    throw std::logic_error("Error: Empty String!");
+  }
+  cstring[size] = '\0';
+  return cstring;
 }
