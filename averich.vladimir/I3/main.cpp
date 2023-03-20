af3ce70f77491bf57e31b49906687351835d24c4
Vavericherry <vaverich3@inbox.ru>
2023-03-21 00:34:33 +0300

    Fix leak

--- averich.vladimir/I3/main.cpp
+++ averich.vladimir/I3/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+    #include <iostream>
 #include <cstddef>
 #include <readthestring.h>
 #include "hasdoubledchars.h"
@@ -10,7 +10,7 @@ int main()
   char* inputString = readTheString(std::cin, MaxStringSize);
   if (!inputString || *inputString == '\0') {
     std::cerr << "Input string is empty" << '\n';
-    if (!inputString) {
+    if (inputString) {
       delete[] inputString;
     }
     return EXIT_FAILURE;
