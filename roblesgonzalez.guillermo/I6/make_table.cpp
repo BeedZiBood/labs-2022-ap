926d1601e79532d803a2fb9773da4e166e18ddc1
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-21 21:39:05 +0300

    Lab 6 done

--- roblesgonzalez.guillermo/I6/make_table.cpp
+++ roblesgonzalez.guillermo/I6/make_table.cpp
@@ -1,21 +1,19 @@
 #include "make_table.h"
-#include <iomanip>
 #include "cosx.h"
+#include <iomanip>
 #include <iostream>
+#include <cmath>
 void printTableRow(std::ostream& out, double x, double cos_error, size_t cos_members)
 {
-  out << std::left << std::setw(10) << std::setfill(' ') << x;
-  out << std::right << std::setw(15) << std::setfill(' ') << std::setprecision(5)
-      << cosx(x, cos_error, cos_members) << '\n';
+  out << std::setw(5) << x << ' ';
+  out << std::setw(10) << std::setprecision(5) << cosx(x, cos_error, cos_members) << ' ';
+  out << std::setw(10) << std::setprecision(5) << std::cos(x) << '\n';
 }
 void printTable(std::ostream& out, double cos_error, size_t cos_members, double left, double right, double step)
 {
-  const int col_width = 15;
-  out << std::left << std::setw(col_width) << "x" << std::setw(col_width) << "f(x)" << '\n';
-  out << std::left << std::setw(col_width) << std::setfill('-') << ""
-      << std::setw(col_width) << std::setfill('-') << ">  out << std::fixed << std::setprecision(5)" << '\n';
-  for (double x = left; x <= right; x += step)
+  for (double x = left; x + step <= right; x += step)
   {
-    out << std::left << std::setw(col_width) << x << std::setw(col_width) << cosx(x, cos_error, cos_members) << '\n';
+    printTableRow(out, x, cos_error, cos_members);
   }
+  printTableRow(out, right, cos_error, cos_members);
 }
