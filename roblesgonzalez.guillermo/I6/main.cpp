77d4c264636f017a6f5ef41d70da75f3004e9c86
guillermo9559 <javier@LAPTOP-D83M1BKQ>
2023-03-21 20:54:05 +0300

    Lab 6

--- roblesgonzalez.guillermo/I6/main.cpp
+++ roblesgonzalez.guillermo/I6/main.cpp
@@ -4,8 +4,8 @@ int main()
 {
   const double step = 0.05;
   const double cos_error = 0.0001;
-  double left_border = 0.0;
-  double right_border = 0.0;
+  double left = 0.0;
+  double right = 0.0;
   size_t cos_members = 0;
   std::cin >> left >> right >> cos_members;
   if (!std::cin || left <= -1 || right >= 1 || right < left)
