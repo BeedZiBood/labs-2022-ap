52f44540d86a6a235ad4a4090793db11bbbb8057
Vavericherry <vaverich3@inbox.ru>
2023-03-21 18:53:23 +0300

    Fix CG

--- averich.vladimir/common/readTheString.cpp
+++ averich.vladimir/common/readTheString.cpp
@@ -3,15 +3,15 @@
 #include <string>
 char* readTheString(std::istream& stream)
 {
-	std::string string;
-	std::getline(std::cin, string);
-	char* buf = new char[string.size() + 1];
-	if (buf) {
-		char const* p = string.c_str();
-		for (size_t i = 0; i < string.size() + 1; ++i) {
-			buf[i] = p[i];
-		}
-	}
-	return buf;
+  std::string string;
+  std::getline(std::cin, string);
+  char* buf = new char[string.size() + 1];
+  if (buf) {
+    char const* p = string.c_str();
+    for (size_t i = 0; i < string.size() + 1; ++i) {
+      buf[i] = p[i];
+    }
+  }
+  return buf;
 }
 
