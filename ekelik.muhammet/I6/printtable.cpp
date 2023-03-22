967142a4607a0256e67df12c042e0d397e62ef3e
mekel1k <mekel1k4444@gmail.com>
2023-03-22 18:31:54 +0300

    fix

--- ekelik.muhammet/I6/printtable.cpp
+++ ekelik.muhammet/I6/printtable.cpp
@@ -10,11 +10,14 @@ void printTableRow(std::ostream& out, double x, double absError, unsigned maxNum
 {
   out << std::setw(5) << x << " ";
   out << std::setw(10) << std::setprecision(precision) << arctan(x, absError, maxNumber) << " ";
-  out << std::setw(10) << std::setprecision(precision) << std::atan(x) << "\n";
+  out << std::setw(10) << std::setprecision(precision) << std::atan(x);
 }
 
 void printTable(std::ostream& out, double m1, double m2, double mStep, unsigned maxNumber, double absError, int precision)
 {
+  out << std::setw(5) << "x" << " ";
+  out << std::setw(10) << "arctan(x)" << " ";
+  out << std::setw(10) << "atan(x)" << "\n";
   if (m1 > m2)
   {
     throw std::logic_error("Borders error");
