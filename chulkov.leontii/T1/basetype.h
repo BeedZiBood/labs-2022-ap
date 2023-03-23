cba636e82ca2ab950db456968d696bbc0fef47c4
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-23 18:50:14 +0300

    correction basetype

--- chulkov.leontii/T1/basetype.h
+++ chulkov.leontii/T1/basetype.h
@@ -1,19 +1,22 @@
 #ifndef BASETYPE_H
 #define BASETYPE_H
 
-struct point_t
+namespace chulkov
 {
-  double x;
-  double y;
-};
+  struct point_t
+  {
+    double x;
+    double y;
+  };
 
-struct rectangle_t
-{
-  point_t pos_;
-  double width_;
-  double height_;
-};
+  struct rectangle_t
+  {
+    point_t pos_;
+    double width_;
+    double height_;
+  };
 
-point_t movePoint(point_t p, point_t dp);
+  point_t movePoint(point_t p, point_t dp);
+}
 
 #endif
