b1c9797339e5321114a16577be48df39e0e42d8a
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-03-23 18:51:33 +0300

    add shape.cpp and correction im shape.h

--- chulkov.leontii/T1/shape.h
+++ chulkov.leontii/T1/shape.h
@@ -3,16 +3,20 @@
 #include <iomanip>
 #include "basetype.h"
 
-class Shape
+namespace chulkov
 {
+  class Shape
+  {
   public:
     virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t& pos) = 0;
     virtual void move(const double dx, const double dy) = 0;
-    virtual void scale(double k) = 0;
+    virtual void unsafeScale(double k) = 0;
     virtual Shape* clone() const = 0;
-};
+    void scale(double k);
+  };
+}
 
 #endif
