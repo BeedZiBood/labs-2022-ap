f5357dca447c04d52a9531a7175cdd5d075701e8
Vavericherry <vaverich3@inbox.ru>
2023-03-26 15:09:49 +0300

    Fix main

--- averich.vladimir/I3/main.cpp
+++ averich.vladimir/I3/main.cpp
@@ -6,19 +6,10 @@
 
 int main()
 {
-  char* inputString = nullptr;
   try
   {
+    char* inputString = new char[];
     inputString = readTheString(std::cin);
-  }
-  catch (const std::exception& e)
-  {
-    std::cout << e.what() << '\n';
-    delete inputString;
-    return EXIT_FAILURE;
-  }
-  try
-  {
     char* resultOfDuplicateTestString = new char[LettersInEnglishAlphabet + 1];
     for (size_t i = 0; i < LettersInEnglishAlphabet + 1; ++i) {
       *(resultOfDuplicateTestString + i) = '\0';
@@ -26,14 +17,14 @@ int main()
     const char predifinedString[] = "abZ";
     findDuplicateLetters(resultOfDuplicateTestString, inputString, predifinedString);
     std::cout << "All chars of cstring: " << resultOfDuplicateTestString << '\n';
+    std::cout << "Repeating chars: " << std::boolalpha << hasDoubledChars(inputString) << '\n';
     delete[] resultOfDuplicateTestString;
+    delete[] inputString;
   }
   catch (const std::bad_alloc &e)
   {
-    delete[] inputString;
+    std::cout << e.what() << '\n';
     return EXIT_FAILURE;
   }
-  std::cout << "Repeating chars: " << std::boolalpha << hasDoubledChars(inputString) << '\n';
-  delete[] inputString;
   return EXIT_SUCCESS;
 }
