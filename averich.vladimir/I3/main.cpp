0db31381e4b2c11be5df4c1cf6f18516863a95fa
Vavericherry <vaverich3@inbox.ru>
2023-03-26 14:00:38 +0300

    Fix leak

--- averich.vladimir/I3/main.cpp
+++ averich.vladimir/I3/main.cpp
@@ -22,10 +22,14 @@ int main()
     delete[] inputString;
     return EXIT_FAILURE;
   }
-  char* resultOfDuplicateTestString = new char[LettersInEnglishAlphabet + 1];
-  if (!resultOfDuplicateTestString) {
-      delete[] inputString;
-      return EXIT_FAILURE;
+  try
+  {
+    char* resultOfDuplicateTestString = new char[LettersInEnglishAlphabet + 1];
+  }
+  catch (const std::bad_alloc &e)
+  { 
+    delete[] inputString;
+    return EXIT_FAILURE;
   }
   for (size_t i = 0; i < LettersInEnglishAlphabet + 1; ++i) {
     *(resultOfDuplicateTestString + i) = '\0';
