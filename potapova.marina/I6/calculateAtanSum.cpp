d8416fdbaf6569918f135e004eb568e583f266a8
Потапова <marina-potapova04@mail.ru>
2023-03-26 11:01:33 +0300

    fix CG

--- potapova.marina/I6/calculateAtanSum.cpp
+++ potapova.marina/I6/calculateAtanSum.cpp
@@ -12,13 +12,13 @@ double calculateAtanSum(double x, double abs_error, size_t num_max)
   double summation = x;
   size_t num_summation = 1;
   double sum_res = 0.0;
-  while (fabs(summation) > abs_error)
+  while (std::fabs(summation) > abs_error)
   {
     if (num_summation > num_max)
     {
       throw std::logic_error("The num_summation doesn't converge");
     }
-    summation = numerator / static_cast<double>(2 * num_summation - 1);
+    summation = numerator / static_cast< double >(2 * num_summation - 1);
     sum_res += summation;
     numerator *= x * x;
     if ((num_summation & 1) == 1)
