61d26e2187e0a737943d489a72a378e07fd7296b
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:09:50 +0300

    Delete kotova.yaroslava directory

--- kotova.yaroslava/I5/digit.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef DIGIT_H
-#define DIGIT_H
-
-bool isFloat(const char* data);
-#endif
--- kotova.yaroslava/I6/calculateExp.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef CALCULATEEXP_HPP
-#define CALCULATEEXP_HPP
-double exp(double x, double absError, unsigned numberMax);
-#endif
