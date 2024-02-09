e6458ad68e66810fab5ebd5e45360c98ca68a526
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 19:58:03 +0300

    Delete fesenko.ivan directory

--- fesenko.ivan/T1/workWithArray.h
+++
@@ -1,7 +0,0 @@
-#ifndef WORKWITHARRAY_H
-#define WORKWITHARRAY_H
-#include <cstddef>
-#include "shape.h"
-Shape **expandArray(Shape **shapes, size_t &capacity);
-void deleteArray(Shape **shapes, size_t capacity);
-#endif
