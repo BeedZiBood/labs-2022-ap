3c4581cc7b05e4e804bd102c4734110f6d555041
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:13:26 +0300

    Delete mishanina.elena directory

--- mishanina.elena/T1/printShapes.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINT_SHAPES_H
-#define PRINT_SHAPES_H
-#include <iomanip>
-#include <istream>
-#include "CompositeShape.h"
-
-void printShapes(std::ostream& out, const CompositeShape& compositeShape);
-
-#endif
