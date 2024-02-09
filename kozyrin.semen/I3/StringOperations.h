c799cd3da210f88518f8c297244705c10f170836
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:10:02 +0300

    Delete kozyrin.semen directory

--- kozyrin.semen/I3/StringOperations.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRINGOPERATIONS_H
-#define STRINGOPERATIONS_H
-#include <iostream>
-
-void switchCase(const char* src, char* dest);
-void threeMostCommon(const char* src, char* dest);
-
-#endif
--- kozyrin.semen/I5/IsReal.h
+++
@@ -1,6 +0,0 @@
-#ifndef ISREAL_H
-#define ISREAL_H
-
-bool isReal(const char* res);
-
-#endif
