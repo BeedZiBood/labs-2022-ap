d3b7829d077304cb96adbb0de3e830843ca59952
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 20:14:54 +0300

    Delete roblesgonzalez.guillermo directory

--- roblesgonzalez.guillermo/I6/make_table.h
+++
@@ -1,5 +0,0 @@
-#ifndef SPBSPU_LABS_2022_AAP_904_A_MAKE_TABLE_H
-#define SPBSPU_LABS_2022_AAP_904_A_MAKE_TABLE_H
-#include <iostream>
-void printTable(std::ostream& out, double cos_error, size_t cos_members, double left, double right, double step);
-#endif
