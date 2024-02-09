91e09bc01ff5f2f4f795df7cd14a913d9e4d25f6
BeedZiBood <117881296+BeedZiBood@users.noreply.github.com>
2024-02-09 19:58:25 +0300

    Delete grishin.vadim directory

--- grishin.vadim/I1/isLocalMax.h
+++
@@ -1,4 +0,0 @@
-#ifndef ISLOCALMAX_H
-#define ISLOCALMAX_H
-int isLocalMax(int currentValue, int value1, int value2);
-#endif
--- grishin.vadim/I1/isSequence.h
+++
@@ -1,4 +0,0 @@
-#ifndef ISSEQUENCE_H
-#define ISSEQUENCE_H
-int isSequence(int currentValue, int value1, int value2);
-#endif
